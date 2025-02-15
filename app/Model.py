e4bc6194660db2161fdeb2079770717418869b12
miguel-merlin <mmerlin@stevens.edu>
2025-02-15 01:04:31 -0800

    feat: added datacom and model files

--- app/Model.py
+++ app/Model.py
@@ -1,3 +1,12 @@
+from crunchflow.input.blocks import Runtime
+
+
 class Model:
     def __init__(self):
         pass
+
+    @staticmethod
+    def create_input():
+        """
+        Creates a new crunflow input tensor.
+        """
