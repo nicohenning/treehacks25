ea4488cdc38c1a7e707be4e364e89d6c25cc0bcc
miguel-merlin <mmerlin@stevens.edu>
2025-02-15 16:19:01 -0800

    feat: changed vite port

--- main/app.py
+++ main/app.py
@@ -8,7 +8,7 @@ logger = setup_logger(__name__)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
